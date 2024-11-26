57c12cb99e02ec60647b5fc804f29567ee6ef50b
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-11-26 19:27:11 +0530

    add initial demo projects

---
+++ springinidemo/src/main/java/com/example/springinidemo/SpringinidemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springinidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringinidemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringinidemoApplication.class, args);
+	}
+
+}
