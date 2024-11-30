f6363cfabd51512455fe15c8194eb769d76e9316
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-11-30 11:15:19 +0530

    add requ

--- requestparameter/src/main/java/com/example/requestparameter/MyController.java
+++ requestparameter/src/main/java/com/example/requestparameter/MyController.java
@@ -10,4 +10,24 @@ public class MyController {
     public double add(@RequestParam(name = "n1") double x, @RequestParam(value = "n2") double y) {
         return x + y;
     }
+
+    @RequestMapping("/subtract")
+    public double subtract(@RequestParam(name = "n1", defaultValue = "10") double x,
+            @RequestParam(value = "n2", defaultValue = "20") double y) {
+        return x - y;
+    }
+
+    @RequestMapping("/prod")
+    public double prod(@RequestParam(name = "n1", defaultValue = "10", required = false) double x,
+            @RequestParam(value = "n2", defaultValue = "20", required = false) double y) {
+        return x * y;
+    }
 }
+
+// @RequestParam can take 3 arguments
+/*
+ * name/value is used name the paramaters
+ * defaultValue to assign the default value to a parameter (defaultValue when
+ * mentioned makes the required false implicitly)
+ * required by default is true
+ */
