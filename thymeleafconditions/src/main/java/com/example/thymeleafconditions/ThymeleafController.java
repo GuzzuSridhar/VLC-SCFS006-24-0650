b1e4b3e07934b78bae1ebc0703725c060116df70
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-12-02 18:39:24 +0530

    add form

--- thymeleafconditions/src/main/java/com/example/thymeleafconditions/ThymeleafController.java
+++ thymeleafconditions/src/main/java/com/example/thymeleafconditions/ThymeleafController.java
@@ -8,8 +8,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class ThymeleafController {
     @RequestMapping("/testage")
-    public String showTest(Model m, @RequestParam("age") int x) {
+    public String showTest(Model m,
+            @RequestParam("name") String n, @RequestParam("age") int x) {
         m.addAttribute("age", x);
+        m.addAttribute("name", n);
         return "welcome";
     }
 }
