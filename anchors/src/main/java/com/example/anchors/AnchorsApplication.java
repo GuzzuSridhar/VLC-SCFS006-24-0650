0d023923e9ccd5dec2bf02b29e4586bf8e45d3a4
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-12-03 16:48:27 +0530

    add anchors

---
+++ anchors/src/main/java/com/example/anchors/AnchorsApplication.java
@@ -0,0 +1,13 @@
+package com.example.anchors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnchorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnchorsApplication.class, args);
+	}
+
+}
---
+++ anchors/src/main/java/com/example/anchors/PageController.java
@@ -0,0 +1,17 @@
+package com.example.anchors;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+    @RequestMapping("/page1")
+    public String showOne() {
+        return "pageone";
+    }
+
+    @RequestMapping("/page2")
+    public String showTwo() {
+        return "pagetwo";
+    }
+}
