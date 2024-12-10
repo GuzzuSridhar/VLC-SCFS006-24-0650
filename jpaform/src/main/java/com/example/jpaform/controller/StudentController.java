adbc5067218dab460c24e2ec50f239e9b365c7e9
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-12-10 17:28:49 +0530

    add list page

--- jpaform/src/main/java/com/example/jpaform/controller/StudentController.java
+++ jpaform/src/main/java/com/example/jpaform/controller/StudentController.java
@@ -1,12 +1,27 @@
 package com.example.jpaform.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.jpaform.repo.StudentRepo;
 
 @Controller
 public class StudentController {
+
+    @Autowired
+    StudentRepo studentRepo;
+
     @GetMapping("/")
     public String showIndex() {
         return "index";
     }
+
+    @GetMapping("/list")
+    public String showStudents(Model model) {
+        model.addAttribute("list", studentRepo.findAll());
+        return "liststudents";
+    }
+
 }
