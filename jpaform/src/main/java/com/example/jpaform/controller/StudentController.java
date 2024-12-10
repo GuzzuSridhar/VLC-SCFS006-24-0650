0c76054843e6cb5c64b41c23d502001e1e3f5292
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-12-10 18:53:41 +0530

    add final

--- jpaform/src/main/java/com/example/jpaform/controller/StudentController.java
+++ jpaform/src/main/java/com/example/jpaform/controller/StudentController.java
@@ -1,9 +1,12 @@
 package com.example.jpaform.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.jpaform.model.Student;
 import com.example.jpaform.repo.StudentRepo;
@@ -36,9 +39,23 @@ public class StudentController {
     public String saveStud(@RequestParam int id,
             @RequestParam String name,
             @RequestParam double math,
-            @RequestParam double eng) {
+            @RequestParam double eng,
+            @RequestParam int grade) {
 
-        studentRepo.save(new Student(id, name, eng, math, id));
+        studentRepo.save(new Student(id, name, eng, math, grade));
+        return "redirect:/list";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showEdit(@PathVariable int id, Model model) {
+        Optional<Student> stuOptional = studentRepo.findById(id);
+        model.addAttribute("stud", stuOptional.get());
+        return "editform";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delStud(@PathVariable int id) {
+        studentRepo.deleteById(id);
         return "redirect:/list";
     }
 
