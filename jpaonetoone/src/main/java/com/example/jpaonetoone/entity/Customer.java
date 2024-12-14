3334ec86c80c0982b574861a223df59e1a7d3354
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-12-14 08:48:34 +0530

    add jpa one to one

--- jpaonetoone/src/main/java/com/example/jpaonetoone/entity/Customer.java
+++ jpaonetoone/src/main/java/com/example/jpaonetoone/entity/Customer.java
@@ -1,5 +1,28 @@
 package com.example.jpaonetoone.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "cust")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    @OneToOne
+    @JoinColumn(name = "add_id") // optional annotation for naming the join column (custom)
+    private Address address;
 
 }
