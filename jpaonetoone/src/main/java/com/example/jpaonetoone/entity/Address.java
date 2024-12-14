1efeba5c074164fb0496f3fa2fbc3fb2022aca25
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-12-14 08:35:55 +0530

    add address class

--- jpaonetoone/src/main/java/com/example/jpaonetoone/entity/Address.java
+++ jpaonetoone/src/main/java/com/example/jpaonetoone/entity/Address.java
@@ -1,5 +1,24 @@
 package com.example.jpaonetoone.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String street;
+    private String State;
+    private String city;
+    private int zip;
 
 }
