6a9bf2c9c2e9d6243c12451435510bfca6fd968a
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-12-14 09:32:23 +0530

    add mapped by

--- jpamanytomany/src/main/java/com/example/jpamanytomany/entity/Course.java
+++ jpamanytomany/src/main/java/com/example/jpamanytomany/entity/Course.java
@@ -1,7 +1,10 @@
 package com.example.jpamanytomany.entity;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,5 +17,7 @@ public class Course {
     @Id
     private long id;
     private String name;
+    @ManyToMany(mappedBy = "courses")
+    private List<Student> students;
 
 }
