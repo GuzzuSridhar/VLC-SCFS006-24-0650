de2ccdc235b84aca5c2c593e6b75697097c46cd0
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-12-17 17:47:52 +0530

    add sec filter chain

--- securityjpa/src/main/java/com/example/securityjpa/security/SecurityConfig.java
+++ securityjpa/src/main/java/com/example/securityjpa/security/SecurityConfig.java
@@ -1,5 +1,41 @@
 package com.example.securityjpa.security;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        return new UserDetailsServiceImpl();
+    }
+
+    @Bean
+    DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login", "/logout", "/").permitAll()
+                        .anyRequest().authenticated());
+        return http.build();
+    }
 }
