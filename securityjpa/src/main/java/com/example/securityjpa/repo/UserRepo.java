4daa36d98782b3cf0f707e8fa80ebed638148cae
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-12-17 17:15:44 +0530

    add Repo

---
+++ securityjpa/src/main/java/com/example/securityjpa/repo/UserRepo.java
@@ -0,0 +1,9 @@
+package com.example.securityjpa.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.securityjpa.model.User;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+
+}
