f9b8f15f1e1c2068849a6d81b6d61f5871eeab58
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-12-21 09:18:21 +0530

    add logs

--- securityjpa/src/main/java/com/example/securityjpa/controllers/UserController.java
+++ securityjpa/src/main/java/com/example/securityjpa/controllers/UserController.java
@@ -1,7 +1,10 @@
 package com.example.securityjpa.controllers;
 
+import java.security.Principal;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -26,38 +29,47 @@ public class UserController {
     // password encryption
     PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
+    Logger logger = LoggerFactory.getLogger("UserController.class");
+
     @GetMapping("/")
     public String showRoot(Model model) {
         List<User> users = userRepo.findAll();
+        logger.info("Full User list queried from DB");
         model.addAttribute("users", users);
         return "welcome";
     }
 
     @PostMapping("/")
-    public String showRoot1(Model model) {
+    public String showRoot1(Model model, Principal principal) {
         List<User> users = userRepo.findAll();
         model.addAttribute("users", users);
+
+        logger.info(principal.getName() + " has logged in");
+
         return "welcome";
     }
 
     @GetMapping("/new")
-    public String showAdd(Model model) {
+    public String showAdd(Model model, Principal principal) {
         User user = new User();
         model.addAttribute("user", user);
         model.addAttribute("roles", roleRepo.findAll());
+        logger.info(principal.getName() + " is attempting to create a  new User ");
         return "newuser";
     }
 
     @GetMapping("/save")
-    public String saveUser(User user) {
+    public String saveUser(User user, Principal principal) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepo.save(user);
+        logger.info(principal.getName() + " has Created a new User " + user.getUserName());
         return "redirect:/";
     }
 
     @GetMapping("/delete/{id}")
-    public String DelUser(@PathVariable("id") long id) {
+    public String DelUser(@PathVariable("id") long id, Principal principal) {
         userRepo.deleteById(id);
+        logger.info(principal.getName() + " has deleted a user ");
         return "redirect:/";
     }
 
